7c9d7b336cfe9e2394982bb331e616d90bd045f8
celani <celani.silva@gmail.com>
2020-10-18 02:35:06 -0300

    updated cmd/root.go

--- cmd/root.go
+++ cmd/root.go
@@ -30,16 +30,12 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "perdat",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Know yourself",
+	Long: `A CLI tool that registers information about your daily life.
+It collects the provided data inputed by the user and stores in a single sqlite file.
+With that data, you can get some insights and know yourself, then, keep learning.`,
+	// if has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -54,14 +50,10 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
+	// Here you define your flags and configuration settings.
+	// persistent flags will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.perdat.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	// local flags will only run when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
